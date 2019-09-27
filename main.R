18a692d7dfb763c162a00ad940259df1f0ad4a1f
yanglinsensjtu <yanglinsensjtu@126.com>
2019-09-27 17:02:11 +0800

    align

--- main.R
+++ main.R
@@ -1,6 +1,24 @@
 library(sangerseqR)
 library(Biostrings)
+library(stringr)
+library(muscle)
 
-a <- readsangerseq('../../sanger 测序结果/GENEID3833-1-M13F.ab1')
+filelist <- dir('../../sanger 测序结果/') %>% 
+  str_subset(pattern = '\\.*.ab1$')
 
-dir('../../sanger 测序结果/')
+seqset <- DNAStringSet()
+
+geneid <- '3833'
+
+pat <- str_c('\\.*',geneid,'\\.*')
+file <- str_subset(filelist, pattern = '\\.*3833\\.*')
+for (i in seq_len(length(file))) {
+  filepath <- str_c('../../sanger 测序结果/',file[i])
+  print(file[i])
+  temp <- readsangerseq(filepath)
+  seq <- as.character(temp@primarySeq)
+  seqset[i] <- seq
+}
+
+aln <- muscle(seqset)
+detail(aln)
