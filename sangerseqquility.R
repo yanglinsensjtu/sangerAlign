e3470fa8c05399e493b295527bc12dc0723f7397
yanglinsensjtu <yanglinsensjtu@126.com>
2019-09-30 16:07:50 +0800

    Computing Sequencing Quality

---
+++ sangerseqquility.R
@@ -0,0 +1,21 @@
+library(sangerseqR)
+g50940.13 <- readsangerseq('../../sanger result/GENEID50940-13-M13F.ab1')
+g50940.14 <- readsangerseq('../../sanger result/GENEID50940-14-M13F.ab1')
+getTrace<- function(traceMatrix){
+  traceMatrix <- tibble::as_tibble(traceMatrix(traceMatrix))
+  colnames(traceMatrix) <- c('A','C','G','T')
+  traceMatrix
+}
+g50940.13 <- getTrace(g50940.13)
+
+
+g50940.14 <- getTrace(g50940.14)
+score.sanger <- function(data){
+ data <- mutate(data, score = (data$A < 5)+
+                               (data$C<5)+
+                               (data$G <5)+ 
+                               (data$`T`<5))
+ data
+}
+g50940.13 <- score.sanger(g50940.13)
+g50940.14 <- score.sanger(g50940.14)
