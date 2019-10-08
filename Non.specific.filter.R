e2a5cc367517afdd611da7534bc299870287d4f0
yanglinsensjtu <yanglinsensjtu@126.com>
2019-10-08 12:35:25 +0800

    remove non-specific to nonspecific folder

---
+++ Non.specific.filter.R
@@ -0,0 +1,32 @@
+library(sangerseqR)
+library(Biostrings)
+library(stringr)
+
+source('sangerseqquility.R')
+sanger.resul.tpath <- '../../sanger result/'
+
+filelist <- dir(sanger.resul.tpath) %>% 
+  str_subset(pattern = '\\.*.ab1$') %>% stringr::str_sort(numeric = T)
+
+nonspecificfolder <- file.path(sanger.resul.tpath, 'nonspecific')
+
+if (file.exists(nonspecificfolder)) {
+  print('The nonspecific folder already existed')
+}else{
+  dir.create(nonspecificfolder)
+}
+
+
+for (i in seq_len(length(filelist))) {
+  filepath <- str_c(sanger.resul.tpath,filelist[i])
+  temp <- readsangerseq(filepath)
+  temp <- score.sanger(temp)
+  score <- mean(temp$score)
+  if (score > 2) {
+    next()
+  }else{
+    file.copy(filepath, nonspecificfolder)
+    file.remove(filepath)
+  }
+  
+}
