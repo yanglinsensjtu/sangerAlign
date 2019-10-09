20cb3f8acddf56f178c4c51266cea90aaad3d831
lsyang <yanglinsensjtu@126.com>
2019-10-09 09:41:07 +0800

    tidy and import primers

---
+++ tidyprimer.R
@@ -0,0 +1,10 @@
+library(Biostrings)
+library(stringr)
+geneid <- '8085'
+primerlist <- dir('../../primer/') %>% str_sort(numeric = T)
+tempfile <- str_subset(primerlist, pattern = geneid)
+primer <- readDNAStringSet(file.path('../../primer/', tempfile))
+primer[2] <- reverseComplement(primer[2])
+names(primer) <- paste(geneid,sep = '.' ,c('Fw','Rv'))
+primer
+
