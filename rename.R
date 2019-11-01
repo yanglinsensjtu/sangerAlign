eabe81a9fd234199b8b99d672092aa54c7f86401
lsyang <yanglinsensjtu@126.com>
2019-11-01 15:02:29 +0800

    rename shenggong sequencing files

--- rename.R
+++ rename.R
@@ -14,3 +14,20 @@ for (i in seq_len(length(file))) {
   file.rename(from = filepath,to = filepathrename)
 }
 
+library(stringr)
+
+sanger.resul.tpath <- '../'
+filelist <- dir(sanger.resul.tpath) %>% 
+  str_subset(pattern = '\\.*.ab1$') %>% 
+  str_sort(numeric = T)
+
+
+
+for (i in seq_len(length(filelist))) {
+  rename <- str_extract(filelist[i], '\\(.*') %>% 
+    str_remove_all('\\)|\\[|\\]|\\(')
+  filepath <- str_c(sanger.resul.tpath,filelist[i])
+  filepathrename <- str_c(sanger.resul.tpath, rename)
+  file.rename(from = filepath,to = filepathrename)
+}
+
